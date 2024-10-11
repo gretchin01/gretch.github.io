4ff90dfdc48e38655c169efe81a6cd720de898cd
Gretchin Ogao <gretchin->
2024-10-11 21:04:38 +0800

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,9 @@
+const body = document.body
+//body.append("Hello", "Day 2")
+
+const div = document.createElement("div")
+body.append(div)
+
+const strong = document.createElement("strong")
+strong.innerText = "Hello"
+div.append(strong)
